4e6559671920ad55e27150b69a43cb865f52ef16
JoseRacine <123jracine@frhd.com>
2022-12-21 10:49:45 -0500

    new code

--- is219-p2/js/gallery.js
+++ is219-p2/js/gallery.js
@@ -33,11 +33,17 @@ function animate() {
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 
 function swapPhoto() {
-	//Add code here to access the #slideShow element.
+	const imageHTML = document.getElementById('photo')
 	//Access the img element and replace its source
+	imageHTML.src = mImages[mCurrentIndex].img;
 	//with a new image from your images array which is loaded 
 	//from the JSON string
-	console.log('swap photo');
+	if(mCurrentIndex >= (mImages.length - 1)){
+		mCurrentIndex = 0;
+	}
+	else{
+		mCurrentIndex++;
+	}
 }
 
 // Counter for the mImages array
@@ -46,15 +52,30 @@ var mCurrentIndex = 0;
 // XMLHttpRequest variable
 var mRequest = new XMLHttpRequest();
 
+
+mRequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+		mJson = JSON.parse(mRequest.responseText);
+		iterateThrough();
+    }
+};
+mRequest.open("GET", mUrl, true);
+mRequest.send();
+
 // Array holding GalleryImage objects (see below).
+
+
+
+
+
 var mImages = [];
 
 // Holds the retrived JSON information
-var mJson;
+var mJson 
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
+var mUrl = '../images.json';
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
@@ -84,5 +105,15 @@ function GalleryImage() {
 	let location = "";
 	let description = "";
 	let date = "";
-	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+	let img = "";
+}
+function iterateThrough()
+{
+	for(const currentIndex in mJson.images){
+		mImages[currentIndex] = new GalleryImage();
+		mImages[currentIndex].img = mJson.images[currentIndex].imgPath;
+		mImages[currentIndex].description = mJson.images[currentImages].description
+		mImages[currentIndex].location = mJson.images[currentIndex].imgLocation;
+		mImages[currentIndex].date = mJson.images[currentIndex].date;
+	}
 }
